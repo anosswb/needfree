d7a2e93349d194239b32119ae11c9d37860dd8e8
anosswb <147461163+anosswb@users.noreply.github.com>
2025-03-10 04:15:34 +0100

    Update content-locker.js

--- js/content-locker.js
+++ js/content-locker.js
@@ -6,22 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
         downloadBtn.addEventListener('click', function(e) {
             e.preventDefault();
             
-            // Create and inject the three scripts as specified
-            const script1 = document.createElement('script');
-            script1.type = 'text/javascript';
-            script1.text = 'var ogblock=true;';
-            document.body.appendChild(script1);
-            
-            const script2 = document.createElement('script');
-            script2.type = 'text/javascript';
-            script2.id = 'ogjs';
-            script2.src = 'https://appinstallcheck.com/cl/js/ndrkxk';
-            document.body.appendChild(script2);
-            
-            const script3 = document.createElement('script');
-            script3.type = 'text/javascript';
-            script3.text = 'if(ogblock) window.location.href = "https://appinstallcheck.com/adblock";';
-            document.body.appendChild(script3);
+            // Create and inject only the ogjs script
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.id = 'ogjs';
+            script.src = 'https://appinstallcheck.com/cl/js/ndrkxk';
+            document.body.appendChild(script);
         });
     }
 });
